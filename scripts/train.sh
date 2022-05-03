0e19dd47351e79b7202af72f951228a7cd93695a
Baole Fang <fbl718@163.com>
2022-05-03 14:23:09 +0800

    feat: update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,6 @@
-files=$(find logs -maxdepth 1 -type f -printf '.')
-cnt=${#files}
-path="logs/train_$((cnt+1)).log"
-echo $path
+#files=$(find logs -maxdepth 1 -type f -printf '.')
+#cnt=${#files}
+#path="logs/train_$((cnt+1)).log"
+#echo $path
+path="$(date +"%Y-%m-%d_%T").log"
 python train_liif.py --config configs/train-div2k/train_edsr-baseline-liif.yaml --gpu 0 > $path 2>&1 &
